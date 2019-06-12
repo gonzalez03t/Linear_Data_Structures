2477120fd86450755f97a1a0b3cde52cccdd0981
Jesus Gonzalez <gonzalez0614@ufl.edu>
2019-06-12 15:53:29 -0400

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linear_Data_Structures
+{
+    class Program
+    {
+        public static void ArrayClass()
+        {
+            //Initiallize array of size 10
+            int[] myArray = new int[10];
+
+            //Populate array
+            Console.WriteLine("Initial Elements: ");
+            for (int i = 0; i < myArray.Length - 4; i++) 
+            {
+                myArray[i] = i * 5;
+                Console.WriteLine("[" + i + "]: " + myArray[i]);
+            }
+            
+            //Testing common array methods
+            Array.Reverse(myArray); //Reverse array
+            Console.WriteLine("After reversing: ");
+            for (int i = 0; i < myArray.Length; i++) //Print reversed array
+            {
+                Console.WriteLine("[" + i + "]: " + myArray[i]);
+            }
+
+            Array.Sort(myArray); //Sort array
+            Console.WriteLine("After sorting: ");
+            for (int i = 0; i < myArray.Length; i++) //Print reversed array
+            {
+                Console.WriteLine("[" + i + "]: " + myArray[i]);
+            }
+
+            Console.WriteLine("Array Lenght: " + myArray.Length); //Returns array lenght
+            Console.WriteLine("Array Type: " + myArray.GetType()); //Returns array type
+            Console.WriteLine("Contains 40? " + myArray.Contains(40)); //Returns true or false if array contains item
+            Console.ReadLine();
+        }
+
+        static void Main(string[] args)
+        {
+            //*****[DATA STRUCTURES]*****//
+            ArrayClass(); //Calling Array Class
+        }
+    }
+}
+.
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Linear_Data_Structures")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Linear_Data_Structures")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4d337546-2c29-4d46-bc03-c9af9c7e81c5")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
